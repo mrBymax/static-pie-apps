e2ddfefea99324a18235853e27b2567642c72ce3
Federico Bertossi <livebymax@gmail.com>
2022-10-23 15:01:45 +0200

    test to run cassandra remotely

---
+++ cassandra/README.md
@@ -0,0 +1,6 @@
+# cassandra
+
+Use the `build.sh` script to download and build `cassandra`.
+The resulting `cassandra` binary is a static PIE executable loadable using the Unikraft ELF Loader.
+
+
--- cassandra/build.sh
+++ cassandra/build.sh
@@ -1,41 +1,10 @@
 #!/bin/bash
 
-APPNAME=cassandra
-VERSION=4.0.6
-DIRNAME=apache-${APPNAME}-${VERSION}
-ARCHIVE=${DIRNAME}-bin.tar.gz
-URL=https://dlcdn.apache.org/${APPNAME}/${VERSION}/${DIRNAME}-bin.tar.gz
+git clone https://github.com/apache/cassandra.git cassandra
+pushd cassandra > /dev/null 2>&1
+git checkout 3.11.10
+ant
+popd > /dev/null 2>&1
 
-# Clean up
-rm -rf ${ARCHIVE}
-rm -rf ${DIRNAME}
-
-echo -n "Downloading ${APPNAME} sources ... "
-wget -q ${URL}
-if test $? -ne 0; then
-  echo ""
-  echo "Unable to download ${APPNAME} from ${URL}"
-  exit 1
-fi
-echo "DONE"
-
-echo ""
-echo -n "Unpacking cassandra ... "
-tar -xzf ${ARCHIVE}
-if test $? -ne 0; then
-  echo ""
-  echo "Unable to extract ${APPNAME}"
-  exit 1
-fi
-echo "DONE"
-
-echo ""
-rm ${ARCHIVE}
-
-echo -n "Building cassandra ... "
-./${DIRNAME}/bin/cassandra
-if test $? -ne 0; then
-  echo ""
-  echo "Unable to build ${APPNAME}"
-  exit 1
-fi
+ln -fn cassandra/build/apache-cassandra-3.11.10/bin/cassandra .
+ln -fn cassandra/build/apache-cassandra-3.11.10/bin/cqlsh .
